2e97bfacd986955152b380782625f4881ad9c879
Raquel Villaverde Palma <raquelv85@gmail.com>
2022-03-14 13:32:41 +0100

    add form and add ngModel to set inputs

--- src/app/dbz/main-page/main-page.component.ts
+++ src/app/dbz/main-page/main-page.component.ts
@@ -1,13 +1,22 @@
 import { Component } from '@angular/core';
 
+interface Personaje {
+  nombre: string;
+  poder: number;
+}
+
 @Component({
   selector: 'app-main-page',
   templateUrl: './main-page.component.html',
 })
-export class MainPageComponent  {
+export class MainPageComponent {
+  nuevo: Personaje = {
+    nombre: 'Trucks',
+    poder: 14000,
+  };
 
-agregar() {
 
-}
-
+  agregar() {
+    console.log(this.nuevo)
+  }
 }
