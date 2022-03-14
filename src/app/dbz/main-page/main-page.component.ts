621d9ddecd9429d48e1336ed09d6215985c293e2
Raquel Villaverde Palma <raquelv85@gmail.com>
2022-03-14 13:19:59 +0100

    add new styles and ngSubmit

--- src/app/dbz/main-page/main-page.component.ts
+++ src/app/dbz/main-page/main-page.component.ts
@@ -1,15 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-main-page',
   templateUrl: './main-page.component.html',
-  styleUrls: ['./main-page.component.css']
 })
-export class MainPageComponent implements OnInit {
+export class MainPageComponent  {
 
-  constructor() { }
+agregar() {
 
-  ngOnInit(): void {
-  }
+}
 
 }
